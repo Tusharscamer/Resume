f276cdb165959d03abb0ffea48dbe5d60762ac50
Tushar Dhakad <tushardhakad355@gmail.com>
2024-06-20 20:55:28 +0530

    add animation

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ let modebut=document.querySelector("#mod");
 let currentMode="Light";
 let body=document.querySelector("body");
 modebut.addEventListener('click',()=>{
-     if(currentMode==="Light"){
+    if(currentMode==="Light"){
         body.classList.add("dark");
         body.classList.remove("white");
         currentMode="Dark";
-     }
-     else{
+    }
+    else{
         body.classList.add("white");
         body.classList.remove("dark");
         currentMode="Light";
-     }
+    }
 })
